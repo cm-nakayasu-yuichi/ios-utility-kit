6459adba8c6ad27415034afd94e5f8bdc0e92faf
Yuichi NAKAYASU <nakayasu.yuichi@classmethod.jp>
2023-03-15 16:32:47 +0900

    ClearButtonクラスを追加

---
+++ Sources/UtilityKit/ViewComponents/ClearButton.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class ClearButton: UIButton {
+    
+    @IBInspectable var highlightedColor: UIColor? = #colorLiteral(red: 0.7498688102, green: 0.7498688102, blue: 0.7498688102, alpha: 0.5)
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                backgroundColor = highlightedColor
+            } else {
+                UIView.transition(
+                    with: self,
+                    duration: 0.25,
+                    options: .curveEaseOut,
+                    animations: { [weak self] in
+                        self?.backgroundColor = .clear
+                    },
+                    completion: nil
+                )
+            }
+        }
+    }
+}
