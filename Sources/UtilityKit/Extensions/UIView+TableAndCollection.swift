5d66ac1e06eaacaabd02c246b9d09110e9aac41d
Yuichi NAKAYASU <nakayasu.yuichi@classmethod.jp>
2023-10-18 22:59:37 +0900

    コレクションビューセルにbindメソッドを追加

--- Sources/UtilityKit/Extensions/UIView+TableAndCollection.swift
+++ Sources/UtilityKit/Extensions/UIView+TableAndCollection.swift
@@ -16,7 +16,30 @@ extension UITableViewCell {
     ///   - type: 指定するセルクラス
     ///   - binding:セルオブジェクトに対して行う処理
     ///
-    public func bind<T>(as type: T.Type, binding: (T) throws -> ()) rethrows {
+    public func bind<T: UITableViewCell>(as type: T.Type, binding: (T) throws -> ()) rethrows {
+        if let cell = self as? T {
+            try binding(cell)
+        }
+    }
+}
+
+extension UICollectionViewCell {
+    
+    /// 再利用されたセルが指定のセルクラスだった場合のみ、そのセルオブジェクトに対して処理を行う
+    ///
+    ///   ```
+    ///   let cell = collectionView.dequeueReusableCell(withIdentifier: row.identifier, for: indexPath)
+    ///   cell.bind(as: BookListCell.self) { cell in
+    ///       cell.book = row.book
+    ///       cell.delegate = self
+    ///   }
+    ///   ```
+    ///
+    /// - Parameters:
+    ///   - type: 指定するセルクラス
+    ///   - binding:セルオブジェクトに対して行う処理
+    ///
+    public func bind<T: UICollectionViewCell>(as type: T.Type, binding: (T) throws -> ()) rethrows {
         if let cell = self as? T {
             try binding(cell)
         }
